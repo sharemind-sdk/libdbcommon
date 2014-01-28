b0eca114272f2daba2b4ee61fef95a7d73035e69
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-01-28 13:55:00 +0200

    dbcommon, ModuleLoader: Removed error-prone LastError logic, other optimizations.

--- ModuleLoader/ModuleLoader.h
+++ ModuleLoader/ModuleLoader.h
@@ -10,61 +10,190 @@
 #ifndef SHAREMIND_DBCOMMON_LOADER_H
 #define SHAREMIND_DBCOMMON_LOADER_H
 
+#include <algorithm>
 #include <cassert>
+#include <cstring>
 #include <map>
 #include <set>
 #include <sharemind/common/ScopedObjectMap.h>
 #include <sharemind/libvm/libvm.h>
+#include <sharemind/likely.h>
 #include <string>
-
-#include "LastError.h"
+#include <vector>
 
 
 namespace sharemind {
 
 namespace moduleLoader {
 
-class Modules;
-
-class ModuleLoader: public LastError {
-
-    friend class Modules;
+template <class Logger>
+class ModuleLoader {
 
 private: /* Types: */
 
+    typedef std::set<std::string> StringSet;
     typedef ScopedObjectMap<std::string, SharemindSyscallBinding> SyscallMap;
     typedef std::map<std::string, SyscallMap> ModuleSyscallMap;
 
 public: /* Methods: */
 
-    ModuleLoader(const std::set<std::string> & signatures);
-    ~ModuleLoader();
+    ModuleLoader(const std::set<std::string> & signatures,
+                 Logger & logger)
+        : m_reqSignatures(signatures)
+        , m_logger(logger.wrap("ModuleLoader: "))
+    {
+        m_modApi = SharemindModuleApi_new();
+        if (unlikely(!m_modApi))
+            throw std::bad_alloc();
+    }
 
-    SharemindModule * addModule(const std::string & filename, const std::string & config = std::string());
+    ~ModuleLoader() {
+        std::for_each(m_modules.begin(),
+                      m_modules.end(),
+                      &SharemindModule_free);
+        m_modules.clear();
+        SharemindModuleApi_free(m_modApi);
+    }
+
+    SharemindModule * addModule(const std::string & filename,
+                                const std::string & config = std::string())
+    {
+        struct GracefulException {};
+        SharemindModule * const m = SharemindModule_new(m_modApi,
+                                                        filename.c_str(),
+                                                        config.c_str());
+        if (unlikely(!m)) {
+            m_logger.error()
+                    << "Error while loading module \"" << filename << "\": "
+                    << SharemindModuleApi_get_last_error_string(m_modApi);
+            return NULL;
+        }
+
+        try {
+            SharemindModuleApiError e = SharemindModule_mod_init(m);
+            if (unlikely(e != SHAREMIND_MODULE_API_OK)) {
+                m_logger.error()
+                        << "Error initializing module: Module returned "
+                        << SharemindModuleApiError_toString(e);
+                throw GracefulException();
+            }
+
+            const char * const moduleName = SharemindModule_get_name(m);
+            assert(moduleName);
+            if (unlikely(m_moduleSyscallMap.count(moduleName))) {
+                m_logger.error() << "Module name \"" << moduleName
+                                 << "\" is already provided by another module.";
+                throw GracefulException();
+            }
+
+            /* Load system calls */
+            const size_t numSyscalls = SharemindModule_get_num_syscalls(m);
+            if (unlikely(numSyscalls != m_reqSignatures.size())) {
+                m_logger.error() << "Invalid number of system calls in module!";
+                throw GracefulException();
+            }
+            for (size_t i = 0u; i < numSyscalls; i++) {
+                const SharemindSyscall * const sc =
+                        SharemindModule_get_syscall(m, i);
+                assert(sc);
+
+                /* Check if system call signature is valid */
+                const char * const scName = SharemindSyscall_get_signature(sc);
+                assert(scName);
+                if (unlikely(!m_reqSignatures.count(scName))) {
+                    m_logger.error() << "The system call \"" << scName
+                                     << "\" is not allowed.";
+                    throw GracefulException();
+                }
+            }
+
+            assert(!m_moduleSyscallMap.count(moduleName));
+            SyscallMap & syscallMap = m_moduleSyscallMap[moduleName];
+            try {
+                for (size_t i = 0u; i < numSyscalls; i++) {
+                    const SharemindSyscall * const sc =
+                            SharemindModule_get_syscall(m, i);
+                    assert(sc);
+                    const char * const scName =
+                            SharemindSyscall_get_signature(sc);
+                    assert(scName);
+
+                    SharemindSyscallBinding * const scBinding =
+                            new SharemindSyscallBinding();
+                    try {
+                        scBinding->wrapper = SharemindSyscall_get_wrapper(sc);
+                        scBinding->moduleHandle = SharemindModule_get_handle(m);
+
+                        std::pair<SyscallMap::iterator, bool> rv =
+                                syscallMap.insert(scName, scBinding);
+                        assert(rv.second);
+                    } catch (...) {
+                        delete scBinding;
+                        throw;
+                    }
+                }
+                m_modules.push_back(m);
+                return m;
+            } catch (...) {
+                m_moduleSyscallMap.erase(moduleName);
+                throw;
+            }
+        } catch (const std::exception & e) {
+            m_logger.error() << "Error loading database module " << filename
+                             << ": " << e.what();
+            SharemindModule_free(m);
+            return NULL;
+        } catch (const GracefulException &) {
+            SharemindModule_free(m);
+            return NULL;
+        }
+    }
 
     inline bool hasModule(const std::string & module) const {
         return m_moduleSyscallMap.find(module) != m_moduleSyscallMap.end();
     }
 
-    const SharemindSyscallBinding * getSyscall(const std::string & module, const std::string & signature) const;
+    const SharemindSyscallBinding * getSyscall(const std::string & module,
+                                               const std::string & signature)
+            const
+    {
+        ModuleSyscallMap::const_iterator msit = m_moduleSyscallMap.find(module);
+        if (msit == m_moduleSyscallMap.end())
+            return NULL;
 
-    inline bool setModuleFacility(const char * name, void * facility, void * context = NULL) {
+        const SyscallMap & syscallMap = msit->second;
+        SyscallMap::const_iterator sit = syscallMap.find(signature);
+        if (sit == syscallMap.end())
+            return NULL;
+
+        return sit->second;
+    }
+
+    inline bool setModuleFacility(const char * name,
+                                  void * facility,
+                                  void * context = NULL)
+    {
         assert(name);
-        return SharemindModuleApi_set_module_facility(m_modapi, name, facility, context);
+        return SharemindModuleApi_set_module_facility(m_modApi,
+                                                      name,
+                                                      facility,
+                                                      context);
     }
 
     inline const SharemindFacility * getModuleFacility(const char * name) {
         assert(name);
-        return SharemindModuleApi_get_module_facility(m_modapi, name);
+        return SharemindModuleApi_get_module_facility(m_modApi, name);
     }
 
 private: /* Fields: */
 
-    SharemindModuleApi * m_modapi;
-    Modules * m_modules;
+    std::vector<SharemindModule *> m_modules;
+    SharemindModuleApi * m_modApi;
     ModuleSyscallMap m_moduleSyscallMap;
 
-    std::set<std::string> m_reqSignatures;
+    StringSet m_reqSignatures;
+
+    typename Logger::Wrapped m_logger;
 
 }; /* class ModuleLoader { */
 
